fd4b9ba4a484dcf47ae89bdd95e9d36f7301c111
liuchiawei <doublecheap@gmail.com>
2026-07-09 18:26:26 +0900

    ui: refactor other pages layout

---
+++ app/[locale]/(others)/layout.tsx
@@ -0,0 +1,3 @@
+export default function OthersLayout({ children }: { children: React.ReactNode }) {
+  return <main className="max-w-5xl 2xl:max-w-7xl mx-auto py-4 flex flex-col items-center justify-center">{children}</main>;
+}
