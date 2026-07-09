cad0ff8f2ec352221ad9edcf80ccd6607c9e19ad
liuchiawei <doublecheap@gmail.com>
2026-07-09 17:52:04 +0900

    feat/hero: add hero component to home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,11 @@
 import { useTranslations } from "next-intl";
+import Hero from "@/components/layout/hero";
 
 export default function Home() {
   const t = useTranslations("Home");
   return (
     <div>
-      <h1>{t("title")}</h1>
+      <Hero />
     </div>
   );
 }
--- components/layout/hero.tsx
+++ components/layout/hero.tsx
@@ -1,7 +1,10 @@
+import { useTranslations } from "next-intl";
+
 export default function Hero() {
+    const t = useTranslations("Home");
     return (
-        <div>
-            <h1>Hero</h1>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1>{t("title")}</h1>
         </div>
     );
 }
