7849fd8639d50b1fcb880658493cc1e129bcd89a
liuchiawei <doublecheap@gmail.com>
2026-07-09 18:33:29 +0900

    refactor: import max-width from config

--- app/[locale]/(others)/layout.tsx
+++ app/[locale]/(others)/layout.tsx
@@ -1,3 +1,6 @@
+import configs from "@/lib/configs";
+import { cn } from "@/lib/utils";
+
 export default function OthersLayout({ children }: { children: React.ReactNode }) {
-  return <main className="max-w-5xl 2xl:max-w-7xl mx-auto py-4 flex flex-col items-center justify-center">{children}</main>;
+  return <main className={cn(configs.maxWidth, "mx-auto py-4 flex flex-col items-center justify-center")}>{children}</main>;
 }
