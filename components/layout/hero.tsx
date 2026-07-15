3ac77f462ecad52dc9029c394b2af709ad90e09f
liuchiawei <doublecheap@gmail.com>
2026-07-15 20:36:30 +0900

    refactor: update Hero component for improved subtitle animation and restructure PortfolioGrid to use a separate PortfolioGridItem component

--- components/layout/hero.tsx
+++ components/layout/hero.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useTranslations } from "next-intl";
-import { motion } from "motion/react";
 import Image from "next/image";
+import { motion } from "motion/react";
 import { cn } from "@/lib/utils";
 import configs from "@/lib/configs";
 
@@ -10,32 +10,20 @@ export default function Hero() {
   const t = useTranslations("Home");
   return (
     <div className="flex flex-col items-center justify-center h-screen relative">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-4">
-        <motion.h1
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          className="text-xl xl:text-2xl 2xl:text-4xl font-serif font-bold text-white text-center text-shadow-lg tracking-widest [writing-mode:vertical-lr]"
-        >
-          {configs.siteTiltle}
-        </motion.h1>
-        <motion.h2
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          className="text-sm xl:text-base 2xl:text-lg font-roboto font-light text-white text-center text-shadow-lg tracking-wide"
-        >
-          {configs.siteSubtitle}
-        </motion.h2>
-      </div>
+      <motion.h1
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.1, ease: "easeOut" }}
+        className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-3xl md:text-5xl lg:text-[3rem] xl:text-[5rem] font-light text-center text-white text-shadow-lg tracking-widest uppercase"
+      >
+        {configs.siteSubtitle}
+      </motion.h1>
       <Image
         src="/images/assets/hero.jpg"
         alt="Hero"
         width={1200}
         height={1200}
-        className={cn(
-          "w-full aspect-video lg:aspect-auto lg:h-full object-cover -z-10",
-        )}
+        className={cn("w-full h-full object-cover -z-10")}
       />
     </div>
   );
--- components/portfolio/portfolio-grid.tsx
+++ components/portfolio/portfolio-grid.tsx
@@ -6,31 +6,37 @@ import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 export function PortfolioGrid() {
   return (
-    <div className="grid grid-cols-4 lg:grid-cols-8">
+    <div className="grid grid-cols-4 lg:grid-cols-6 gap-4">
       {Array.from({ length: configs.portfolio.totalVolume }, (_, i) => (
-        <div key={i} className="w-full h-full">
-          <Dialog>
-            <DialogTrigger className="w-full h-full p-0">
-              <Image
-                src={`/images/portfolio/${i + 1}.jpg`}
-                alt={`Portfolio ${i + 1}`}
-                width={100}
-                height={100}
-                className="w-full h-full object-cover"
-              />
-            </DialogTrigger>
-            <DialogContent showCloseButton={false} className="p-0">
-              <Image
-                src={`/images/portfolio/${i + 1}.jpg`}
-                alt={`Portfolio ${i + 1}`}
-                width={800}
-                height={800}
-                className="w-full h-full object-cover"
-              />
-            </DialogContent>
-          </Dialog>
-        </div>
+        <PortfolioGridItem key={i} i={i} />
       ))}
     </div>
   );
 }
+
+const PortfolioGridItem = ({ i }: { i: number }) => {
+  return (
+    <div key={i} className="size-full aspect-square">
+      <Dialog>
+        <DialogTrigger className="size-full p-0">
+          <Image
+            src={`/images/portfolio/${i + 1}.jpg`}
+            alt={`Portfolio ${i + 1}`}
+            width={100}
+            height={100}
+            className="w-full h-full object-cover"
+          />
+        </DialogTrigger>
+        <DialogContent showCloseButton={false} className="p-0">
+          <Image
+            src={`/images/portfolio/${i + 1}.jpg`}
+            alt={`Portfolio ${i + 1}`}
+            width={800}
+            height={800}
+            className="w-full h-full object-cover"
+          />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
